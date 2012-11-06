bb9815dc8f69fcdeda1bec51197cb5cf35a6d7b3
Blake Thomson <thomsbg@gmail.com>
2012-11-06 14:02:59 -0500

    Initial import

---
+++ ToGa/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToGa
+//
+//  Created by Blake Thomson on 8/21/12.
+//  Copyright (c) 2012 Blake Thomson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+@end
